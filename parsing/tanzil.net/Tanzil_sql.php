9c1cb195cfc0f74cc72117a6e038dd9c38a2b59f
codeforweb <abuazraa@gmail.com>
2016-02-17 11:19:01 +0530

    add parsing from tanzil and urdhu file

---
+++ parsing/tanzil.net/Tanzil_sql.php
@@ -0,0 +1,29 @@
+﻿<?php
+/*
+$lines = file('en.hilali');
+$sql='';
+foreach ($lines as $line_num => $line) {
+	list($SuraID, $VerseID, $AyahText) = explode('|',$line);
+	$AyahText=addslashes(trim($AyahText));
+	$sql .= "INSERT INTO `Quran` (`DatabaseID`,`SuraID`,`VerseID`,`AyahText`) VALUES (3,$SuraID,$VerseID,'$AyahText');"."\n";
+}
+	echo 'DONE';
+	file_put_contents('3_mk.sql', $sql);
+
+	
+*/
+
+$lines = file('ur.junagarhi.txt');
+$sql='';
+foreach ($lines as $line_num => $line) {
+	list($SuraID, $VerseID, $AyahText) = explode('|',$line);
+	$AyahText=addslashes(trim($AyahText));
+	$sql .= "INSERT INTO `Quran` (`DatabaseID`,`SuraID`,`VerseID`,`AyahText`) VALUES (11,$SuraID,$VerseID,'$AyahText');"."\n";
+}
+	echo 'DONE';
+	file_put_contents('11_MuhammadJunagarhi.sql', $sql);
+//*/
+
+
+
+?>
