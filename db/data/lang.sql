216fefec61a4d44a406249e59fc594b17f32145a
codeforweb <abuazraa@gmail.com>
2020-05-23 17:28:52 +0530

    Update DB

--- db/data/lang.sql
+++ db/data/lang.sql
@@ -17,10 +17,12 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Dumping data for table `lang`
 --
 
+
 INSERT INTO `lang` (`ID`, `DatabaseID`, `Language`, `Orientation`, `Name`, `Translator`) VALUES
 (1, 1, 'Arabic', 'RTL', 'King Fahad Quran Complex', ''),
 (2, 2, 'English-Transliteration', 'LTR', '', ''),
 (3, 3, 'English', 'LTR', 'Hilali & Khan', '	Muhammad Taqi-ud-Din al-Hilali and Muhammad Muhsin Khan'),
 (4, 4, 'English', 'LTR', 'Saheeh International', ''),
 (5, 5, 'Tamil', 'LTR', 'தாருல் ஹுதா', 'ஆ.கா.அப்துல் ஹமீது பாகவி'),
-(6, 6, 'Tamil', 'LTR', 'ஜான் டிரஸ்ட் நிறுவனம்', 'டாக்டர். முஹம்மது ஜான்');
+(6, 6, 'Tamil', 'LTR', 'ஜான் டிரஸ்ட் நிறுவனம்', 'டாக்டர். முஹம்மது ஜான்'),
+(7, 7, 'Tamil', 'LTR', 'மன்னர் ஃபஹத் வளாகம் (சவூதி)', '');
